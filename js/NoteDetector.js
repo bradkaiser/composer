8926816b2166485a747cdb0bfe835f644d8507ef
Brad Kaiser <brad.kaiser@pamlabdev.com>
2013-11-17 02:16:16 -0500

    improved note detection

--- js/NoteDetector.js
+++ js/NoteDetector.js
@@ -1,3 +1,38 @@
+//convenience methods
+Object.defineProperty( Array.prototype, "last", {
+    enumerable: false,
+    configurable: false,
+    writable: false,
+    value: function() {
+        return this[ this.length - 1 ];
+    }
+} );
+
+Object.defineProperty( Array.prototype, "splitOn", {
+    enumerable: false,
+    configurable: false,
+    writable: false,
+    value: function(pred) {
+        var result = [];
+        var isNewSegment = true;
+
+        this.forEach(function(x) {
+            if(!pred(x)) {
+                if(isNewSegment) {
+                    result.push([x]);
+                } else {
+                    result.last().push(x);
+                }
+                isNewSegment = false;
+            } else {
+                isNewSegment = true;
+            }
+        });
+
+        return result;
+    }
+});
+
 var ComposerAudio = (function(ComposerAudio, Module) {
 
     //private static properties and methods
@@ -16,6 +51,12 @@ var ComposerAudio = (function(ComposerAudio, Module) {
             {type: {duration: 16, durationmodifier: "d"}, value: 3},
             {type: {duration: 16, durationmodifier: ""}, value: 2}
     ];
+
+    var mean = function(xs) {
+        var sum = xs.reduce(function(acc, x) { return acc + x; }, 0);
+
+        return Math.round(sum / xs.length);
+    }
 
     var log2 = function(x) {
         return Math.log(x) / Math.LN2;
@@ -38,12 +79,20 @@ var ComposerAudio = (function(ComposerAudio, Module) {
         return result;
     };
 
+    var isClose = function(x,y) {
+        return Math.abs(x - y) < 3;
+    }
+
 
     function NoteDetector(bufferSize, bpm) {
         //public properties
         this.bufferSize = bufferSize;
         this.bpm = bpm;
         this.middleAFreq = 440;
+        this.durationOfSample = bufferSize / 44100 * 1000;
+        // 120 b/m * 1/60 m/s = 2 b/s -> 1/2 s/b * 1/4 b/sixteenths = 1/8 s = 125 ms
+        this.msInSixteenth = (bpm / 60) / 16 * 1000;           
+        this.samplesInSixteenth = Math.floor(this.msInSixteenth / this.durationOfSample);
 
         //private properties
         var on = false;
@@ -53,6 +102,7 @@ var ComposerAudio = (function(ComposerAudio, Module) {
         var trackerPtr = Module._malloc(12);
         Module._dywapitch_inittracking(trackerPtr);
         var that = this;
+
 
         //for now we are assuming 4 4 time. One beat is a quarter note
         var msPerQuarter = (60 / this.bpm) * 1000;
@@ -93,6 +143,31 @@ var ComposerAudio = (function(ComposerAudio, Module) {
             return result;
         }
         
+        //don't break up note too delicately, just get the largest chunk that will fit.
+        var msToLargestNoteType = function(duration) {
+            //largest note we return is whole note
+            //smallest note we return in 16th note
+            console.log('duration ' + duration);
+            var thirtySecondNotes = Math.round(duration / msPer32nd);
+            var result = [];
+
+            noteTypeList.forEach( function(noteType) {
+                    var occurrences = Math.floor( thirtySecondNotes / noteType.value );
+                    thirtySecondNotes = thirtySecondNotes % noteType.value;
+
+                    result = result.concat(repeat(occurrences, noteType.type));
+            });
+
+            //round up to 16th
+            if (result.length === 0) {
+                return {duration: 16, durationmodifier: ""};
+            } else {
+                return result[0];
+            }
+
+
+        }
+
         var rawEventToNotes = function(raw) {
             var types = msToNoteType(raw.duration);
             var keyNumber = freqToKeyNumber(raw.freq);
@@ -135,22 +210,16 @@ var ComposerAudio = (function(ComposerAudio, Module) {
             return result;
         }
 
-         var compressOctaves = function(note) { 
-            if (note.octave > 5) {
-                note.octave = 5;
-            }
 
-            if (note.octave < 4) {
-                note.octave = 4;
-            }
-        }
+
 
         //public methods
-        this.process = function(rawData) { 
+        this.barf = function(rawData) { 
             var pcm = rawData.data;
             var time = rawData.time;
-            //var maxVol = max(pcm);
+
             var result = rawToPitch(pcm);
+            console.log(result);
 
             if (result.confidence > 1) {
                 if(on) { 
@@ -178,6 +247,91 @@ var ComposerAudio = (function(ComposerAudio, Module) {
                 }
             }
         };
+
+        var lastPublished;
+        var currentKeyNumber;
+        var currentNoteStartTime;
+
+
+
+        //accumulate pitchdetected audio data so we can pull out notes that are longer than cutoff
+        this.generateAccumulator = function(howMany, callBack) {
+            var accumulator = [];
+            var remainingSpots = howMany;
+
+            return function(keyNumObj) {
+                accumulator.push(keyNumObj);
+                remainingSpots--;
+                if (remainingSpots === 0) {
+                    callBack(accumulator);
+                    remainingSpots = howMany;
+                }
+            };
+        };
+
+
+        //format collection of grouped pitched samples and turn them into a note 
+        this.format = function(keyNumGroup) {
+            var duration = keyNumGroup.length * that.durationOfSample;
+            var meanKeyNum = mean(keyNumGroup.map(function(x) { return x.keyNumber; }));
+            var octave = keyNumberToOctave(meanKeyNum);
+            var note = keyNumberToNote(meanKeyNum);
+            var result = msToLargestNoteType(duration);
+            result.octave = octave;
+            result.note = note;
+
+            return result;
+        };
+
+        this.publish = function(note) {
+            var noteEvent = new CustomEvent("noteEvent",{detail: note});
+            window.dispatchEvent(noteEvent);
+        }
+
+        //uses list of pitches and pulls out notes that are at least a sixteenth note. Will leave data at end in case it is
+        //continued in next batch;
+        this.notePuller = function(accumulator) {
+            var workingArray = accumulator.slice(); //make copy 
+            accumulator.splice(0); //clean data out of accumulator
+
+            //copy contiguous keys at end back into accumulator
+            var lastKeyNum = workingArray.last().keyNumber;
+            for(var i = workingArray.length - 1; i >= 0; i--) {
+                if (isClose(workingArray[i].keyNumber, lastKeyNum)) {
+                    accumulator.unshift(workingArray.pop());
+                } else {
+                    break;
+                }
+            }
+
+            //split into groups when no pitch detected
+            var keyNumGroups = workingArray.splitOn(function(x) { return x.keyNumber == -Infinity });
+
+            //reject detected pitches that are shorter than a eigth note
+            var noteClusters = keyNumGroups.filter(function (xs) { return xs.length >= that.samplesInSixteenth });
+            console.log(noteClusters);
+
+            var noteObjs = noteClusters.map(function(x) { return that.format(x); });
+
+            noteObjs.forEach(function(note) { that.publish(note); });
+        };
+
+        this.chunkNotes = this.generateAccumulator(10,this.notePuller);
+
+        this.process = function(rawData) {
+            var pcm = rawData.data;
+            var time = rawData.time;
+
+            var result = rawToPitch(pcm);
+            var confidence = result.confidence;
+            var pitch = result.pitch;
+            var keyNumber = freqToKeyNumber(pitch);
+
+            var keyNumObj = {keyNumber: keyNumber, time: time, confidence: confidence};
+            this.chunkNotes(keyNumObj);
+
+        };
+
 
     }
 
